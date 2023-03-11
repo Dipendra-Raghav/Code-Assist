bff217983197f4b20e2dd408e3a79f6ff3bed174
Dipendra-Raghav <raghavdip19@gmail.com>
2023-03-11 10:17:45 +0530

    Routes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:code_assist/features/auth/screens/login_screen.dart';
+import 'package:code_assist/router.dart';
 import 'package:code_assist/theme/pallete.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
 import 'firebase_options.dart';
 
@@ -22,11 +24,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Code Assist',
       theme: Pallete.darkModeAppTheme,
-      home: const LoginScreen(),
+      routerDelegate:
+          RoutemasterDelegate(routesBuilder: (context) => loggedOutRoute),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
